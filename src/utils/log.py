a6c922f82f4338df64f1a92ccc52c007ee9e12fa
enzoblain <blenzodu57@gmail.com>
2024-12-26 23:15:13 +0100

    Refactor logging mechanism: unify log file naming to 'run.log', enhance error reporting in workflow, and streamline log file handling.

--- src/utils/log.py
+++ src/utils/log.py
@@ -1,20 +1,8 @@
-import os
 import time
 import inspect
 
-def lastLog() -> str:
-    i = 0
-
-    while True:
-        if os.path.exists(f'logs/run_{i}.log'):
-            i += 1
-        else:
-            break
-
-    return i - 1
-
 def initLog() -> str:
-    logFilePath = f'logs/run_{lastLog() + 1}.log'
+    logFilePath = f'logs/run.log'
 
     with open(logFilePath, 'w') as _:
         pass
@@ -23,14 +11,14 @@ def initLog() -> str:
     print(f'Log file created at {logFilePath}')
 
 def addLog(message: str) -> None:
-    logFilePath = f'logs/run_{lastLog()}.log'
+    logFilePath = f'logs/run.log'
     with open(logFilePath, 'a') as file:
         file.write(f'{time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())} : {message}\n')
 
     return
 
 def displayError(error_message: str) -> None:
-    caller_function  = caller = inspect.stack()[1].function
+    caller_function  = inspect.stack()[1].function
     caller_file = inspect.stack()[1].filename
 
     addLog(f"ERROR in '{caller_function}' (file: {caller_file}): {error_message}")
