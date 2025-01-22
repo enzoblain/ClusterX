76b51abc219afaa526d099573877aee464d7b451
enzoblain <blenzodu57@gmail.com>
2025-01-22 05:37:37 +0100

    Update environment configuration to 'test' and add lotSizeCalculator function with error handling for invalid inputs

--- src/indicators.py
+++ src/indicators.py
@@ -23,3 +23,24 @@ def fibonacciRetracement(start: np.float64, end: np.float64) -> List[np.float64]
         displayError(e)
 
     return retracement
+
+def lotSizeCalculator(balance: np.float64, risk: np.float64, stop_loss_pips: np.float64, symbol: str) -> np.float64:
+    if not balance or not risk or not stop_loss_pips or not symbol:
+        displayError("Invalid balance, risk or stopLoss value for lotSizeCalculator")
+
+    pip_values = {
+        "EURUSD": 0.0001,
+    }
+
+    if symbol not in pip_values:
+        displayError(f"Invalid symbol \”{symbol}\”")
+
+    try:
+        risk_amount = balance * (risk / 100)
+        pip_value = pip_values[symbol]
+        lot_size =  risk_amount / (stop_loss_pips * pip_value)
+        
+        return (balance * risk) / stop_loss_pips
+
+    except Exception as e:
+        displayError(e)
