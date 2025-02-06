8e250003ec7141f02ac45b6c1895f1c98263faa5
enzoblain <blenzodu57@gmail.com>
2025-02-06 13:52:17 +0100

    FIX #2: Use config file

--- src/algorithm.py
+++ src/algorithm.py
@@ -1,9 +1,12 @@
 from src.api import getDataFromTwelveDataApi
+from src.utils import getFromConfigFile
 
 def algorithm():
     intervals = ["1min", "5min", "15min", "30min", "1h", "4h", "1day"] # List of intervals
     candles = {} # Store candles for each interval
 
+    symbol = getFromConfigFile("Symbol")
+
     # Get data for each interval
     for interval in intervals: 
-        candles[interval] = getDataFromTwelveDataApi(symbol="EUR/USD", interval=interval)
+        candles[interval] = getDataFromTwelveDataApi(symbol=symbol, interval=interval)
