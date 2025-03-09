0ec7e656a8070f21dfcbbf376582d47e7c7dcc49
enzoblain <blenzodu57@gmail.com>
2025-03-10 01:42:42 +0200

    .gitignore

--- python/build.sh
+++
@@ -1,3 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS="firebase.json"
-
-/opt/homebrew/bin/python3.11 "/Users/enzoblain/Perso/Programmation/Projets/ClusterX/ClusterX - algorithm (python)/main.py"
--- python/src/visualisation.py
+++
@@ -1,8 +0,0 @@
-import mplfinance as mpf
-import pandas as pd
-
-def plot(candles):
-    df = candles.to_pandas()
-    df["datetime"] = pd.to_datetime(df["datetime"])  # Convertir en datetime
-    df.set_index("datetime", inplace=True)
-    mpf.plot(df, type="candle", style="charles", volume=False, title="Candlestick Chart", ylabel="Price")
