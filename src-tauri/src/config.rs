70aea37029dc6f30dbb67d633918f63042bc3a0c
enzoblain <blenzodu57@gmail.com>
2025-03-13 02:37:37 +0200

    FIX #2: Fix bug with duplicate log_number creation

--- src-tauri/src/config.rs
+++ src-tauri/src/config.rs
@@ -1,10 +1,2 @@
-use lazy_static::lazy_static;
-use std::sync::Mutex;
-
 pub static USER_DATA_FOLDER: &str = "../public/data";
-pub static LOG_FOLDER: &str = "../logs";
-
-// Var that should be changed in the code
-lazy_static! {
-    pub static ref log_number: Mutex<u32> = Mutex::new(0);
-}
+pub static LOG_PATH: &str = "../logs/log.log";
